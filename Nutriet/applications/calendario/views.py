4871ef11ce14ddb9ececda85f2b5fa0e3fb40f69
santiago velasco <santi.6velase@gmail.com>
2026-03-11 14:42:34 -0500

    Correccion en views de obtener eventos

--- Nutriet/applications/calendario/views.py
+++ Nutriet/applications/calendario/views.py
@@ -15,8 +15,8 @@ logger = logging.getLogger(__name__)
 @never_cache
 @login_required
 @verificar_formulario_completo
-def calendario_view(request):
-    
+def obtener_eventos(request):
+
     eventos = Actividad.objects.filter(usuario=request.user)
 
     data = []
